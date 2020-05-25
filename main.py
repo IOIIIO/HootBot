f5acb193701181a17656ae10428d3eb87bd13086
IOIIIO <101110@vivaldi.net>
2020-05-26 00:52:33 +0200

    Add Reddit embedding

--- main.py
+++ main.py
@@ -50,6 +50,7 @@ try:
 	from bs4 import BeautifulSoup
 	import requests
 	from imgurpython import ImgurClient
+	import praw
 except:
 	sr()
 
@@ -62,6 +63,13 @@ else:
 
 
 exceptions = []
+try:
+	reddit = praw.Reddit(client_id=cfg["config"]["reddit_id"],
+                     client_secret=cfg["config"]["reddit_scr"],
+                     refresh_token=cfg["config"]["reddit_tkn"],
+                     user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36 Edg/83.0.478.37")
+except:
+	pass
 
 # https://stackoverflow.com/a/45579374
 def get_id(url):
@@ -104,11 +112,13 @@ async def buildEmbed(msg, url, tweet = '', author = ''):
 
 	await bot.get_channel(cfg[str(msg.guild.id)]['bot']['archive_channel']).send(embed=embed)
 
-bot = commands.Bot(command_prefix=cfg["config"]["prefix"])
+bot = commands.Bot(command_prefix=cfg["config"]["prefix"], owner=cfg["owner"])
 
 @bot.event
 async def on_ready():
 	print('Logged in as {}'.format(bot.user.name))
+	if cfg["config"] and cfg["config"]["reddit_id"] and cfg["config"]["reddit_scr"] != "":
+		print('Logged into Reddit as {}'.format(reddit.user.me()))
 	
 	if cfg["config"] and cfg["config"]["presence"] and cfg['config']["presence"] != "":
 		await bot.change_presence(activity=discord.Game(name=cfg["config"]['presence']))
@@ -200,27 +210,35 @@ async def on_raw_reaction_add(payload):
 						else:
 							await buildEmbed(msg, '')
 
+async def return_reddit(url):
+	return str(praw.models.Submission(reddit=reddit, url=url[0][0]).url)
+
 @bot.event
 async def on_message(message):
-	if 'insta' in cfg[str(message.guild.id)]:
-		if cfg[str(message.guild.id)]['insta'] == True:
-			url = re.findall(r'((https?):((//)|(\\\\))+([\w\d:#@%/;$()~_?\+-=\\\.&](#!)?)*)', message.content)
-			if url != []:
-				if "instagram.com" in url[0][0]:
-					embed=discord.Embed(title="Instagram Embed", description=message.content)
-					embed.add_field(name='Sender', value=str(message.author))
-					embed.set_image(url=BeautifulSoup(requests.get(url[0][0].replace('mobile.', '')).text, 'html.parser').find('meta', attrs={'property':'og:image'}).get('content'))
-					await message.channel.send(embed=embed)
+	if not isinstance(message.channel, discord.channel.DMChannel):
+		if 'insta' in cfg[str(message.guild.id)]:
+			if cfg[str(message.guild.id)]['insta'] == True:
+				url = re.findall(r'((https?):((//)|(\\\\))+([\w\d:#@%/;$()~_?\+-=\\\.&](#!)?)*)', message.content)
+				if url != []:
+					if "instagram.com" in url[0][0]:
+						embed=discord.Embed(title="Instagram Embed", description=message.content)
+						embed.add_field(name='Sender', value=str(message.author))
+						embed.set_image(url=BeautifulSoup(requests.get(url[0][0].replace('mobile.', '')).text, 'html.parser').find('meta', attrs={'property':'og:image'}).get('content'))
+						await message.channel.send(embed=embed)
 
-	if 'reddit' in cfg[str(message.guild.id)]:
-		if cfg[str(message.guild.id)]['reddit'] == True:
-			url = re.findall(r'((https?):((//)|(\\\\))+([\w\d:#@%/;$()~_?\+-=\\\.&](#!)?)*)', message.content)
-			if url != []:
-				if "reddit.com" in url[0][0] or "redd.it" in url[0][0]:
-					embed=discord.Embed(title="Reddit Embed", description=message.content)
-					embed.add_field(name='Sender', value=str(message.author))
-					embed.set_image(url=message.embeds[0].thumbnail.url)
-					await message.channel.send(embed=embed)
+		if 'reddit' in cfg[str(message.guild.id)]:
+			if cfg[str(message.guild.id)]['reddit'] == True:
+				url = re.findall(r'((https?):((//)|(\\\\))+([\w\d:#@%/;$()~_?\+-=\\\.&](#!)?)*)', message.content)
+				if url != []:
+					if "reddit.com" in url[0][0] or "redd.it" in url[0][0]:
+						try:
+							embed=discord.Embed(title="Reddit Embed", description=message.content)
+							embed.add_field(name='Sender', value=str(message.author))
+							b = await return_reddit(url)
+							embed.set_image(url=b)
+							await message.channel.send(embed=embed)
+						except:
+							pass
 
 	await bot.process_commands(message)
 
@@ -290,6 +308,22 @@ async def cache(ctx):
 			json.dump(cfg, open('bot.json', 'w'), indent=4)
 			await ctx.send("Succesfully changed cache state to: \"{}\"".format(b))
 
+@cache.command(brief='Set imgur UserID.')
+async def user(ctx, *, b: str):
+	if is_owner(ctx):
+		cfg["config"].update({'imgur_usr' : b})
+		json.dump(cfg, open('bot.json', 'w'), indent=4)
+		await ctx.send("Succesfully updated key.")
+	b = 0
+
+@cache.command(brief='Set imgur Secret.')
+async def secret(ctx, *, b: str):
+	if is_owner(ctx):
+		cfg["config"].update({'imgur_scr' : b})
+		json.dump(cfg, open('bot.json', 'w'), indent=4)
+		await ctx.send("Succesfully updated key.")
+	b = 0
+
 """
 Toggle automatic Instagram embeds.
 """
@@ -314,9 +348,13 @@ async def instagram(ctx):
 	json.dump(cfg, open('bot.json', 'w'), indent=4)
 	await ctx.send("Succesfully changed embed state to: \"{}\"".format(b))
 
-@embed.group(brief='Toggle automatic Reddit embeds.')
-@commands.has_permissions(administrator=True)
-async def reddit(ctx):
+@embed.group(name="reddit", brief="Configure reddit embed settings")
+async def redd(ctx):
+	pass
+	
+#@commands.has_permissions(administrator=True)
+@redd.command(brief='Toggle automatic Reddit embeds.')
+async def enable(ctx):
 	if "reddit" in cfg[str(ctx.message.guild.id)]:
 		if cfg[str(ctx.message.guild.id)]["reddit"] == True:
 			cfg[str(ctx.message.guild.id)].update({'reddit' : False})
@@ -330,18 +368,26 @@ async def reddit(ctx):
 	json.dump(cfg, open('bot.json', 'w'), indent=4)
 	await ctx.send("Succesfully changed embed state to: \"{}\"".format(b))
 
-@cache.group(brief='Set imgur UserID.')
-async def user(ctx, *, b: str):
+@redd.command(brief='Set Reddit ID.')
+async def id(ctx, *, b: str):
 	if is_owner(ctx):
-		cfg["config"].update({'imgur_usr' : b})
+		cfg["config"].update({'reddit_id' : b})
 		json.dump(cfg, open('bot.json', 'w'), indent=4)
 		await ctx.send("Succesfully updated key.")
 	b = 0
 
-@cache.group(brief='Set imgur Secret.')
+@redd.command(brief='Set Reddit Secret.')
 async def secret(ctx, *, b: str):
 	if is_owner(ctx):
-		cfg["config"].update({'imgur_scr' : b})
+		cfg["config"].update({'reddit_scr' : b})
+		json.dump(cfg, open('bot.json', 'w'), indent=4)
+		await ctx.send("Succesfully updated key.")
+	b = 0
+
+@redd.command(brief='Set Reddit Token.')
+async def token(ctx, *, b: str):
+	if is_owner(ctx):
+		cfg["config"].update({'reddit_tkn' : b})
 		json.dump(cfg, open('bot.json', 'w'), indent=4)
 		await ctx.send("Succesfully updated key.")
 	b = 0
