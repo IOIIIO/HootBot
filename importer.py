743203899065264ff67064653206bec87bf3a50a
IOIIIO <101110@vivaldi.net>
2021-09-05 02:38:32 +0200

    Add migration tool

---
+++ importer.py
@@ -0,0 +1,27 @@
+import json
+import os
+import dataset
+
+rows = []
+
+if os.path.isfile("bot.json"):
+	cfg = json.load(open('bot.json'))
+else:
+	print("no importable config found")
+	exit()
+
+for server in cfg:
+	if server not in ["config", "token", "owner"]:
+		for conc in cfg[server]["ignore_list"]:
+			message = conc[18:]
+			channel = conc[:18]
+			print("Found: Server: {} - Channel: {} - Message: {}".format(server, channel, message))
+			rows.append(dict(server_id=server, channel_id=channel, message_id=message))
+
+db = dataset.connect('sqlite:///settingstest.db')
+db.begin()
+try:
+    db["starboardIgnore"].upsert_many(rows, ['message_id'])
+    db.commit()
+except:
+    db.rollback()
